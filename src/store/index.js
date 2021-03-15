558add94c0279bfb45edff2b926b8889c93ee5f4
sachinshrestha <sachinshrestha483@gmail.com>
2021-03-15 08:11:35 +0530

    initial

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,53 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode:'dwdwefrdf',
   },
   mutations: {
+    decreaseCounter(state) {
+      state.counter--;
+    },
+    increaseCounter(state, randomNumber) {
+      console.log(randomNumber);
+
+      state.counter = randomNumber;
+    },
+    setColorCode(state,newValue){
+      state.colorCode=newValue;
+    },
+    checkFun(){
+      console.log("Call Function");
+    }
+    
+
+
   },
   actions: {
+    async decreaseCounter({ commit }) {
+      console.log("Action Occured");
+
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        console.log(response);
+        commit("increaseCounter", response.data);
+      });
+
+      console.log("Await done");
+    },
+    increaseCounter(state) {
+      console.log("Action Occured");
+
+      console.log("Await done");
+    },
   },
-  modules: {
-  }
-})
+  getters:{
+counterSquared(state){
+  return state.counter*state.counter
+}
+  },
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ $store.state.counter }}</h1>
+    <br />
+    <br />
+    <div>
+      {{ $store.state.counter }}
+      <sup>2</sup>
+      =
+      {{ $store.getters.counterSquared }}
+    </div>
+
+    <p>
+      <button v-on:click="$store.commit('increaseCounter')">
+        + Add The Counter
+      </button>
+      <br />
+      <br />
+
+      <button v-on:click="$store.commit('decreaseCounter')">
+        - Subtract The Counter
+      </button>
+    </p>
+    <p></p>
+    <button v-on:click="$store.dispatch('increaseCounter')">
+      + Add The Counter ny Actions
+    </button>
+
+    <br />
+    <br />
+
+    <button v-on:click="$store.dispatch('decreaseCounter')">
+      - Subtract The Counter by Actions
+    </button>
+
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <div>
+      <h1>{{ $store.state.colorCode }}</h1>
+
+      <input v-model="colorCode" type="text" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { useStore } from "vuex";
 
+// @ is an alias to /src
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  computed: {
+    colorCode: {
+      get() {
+        return this.$store.state.colorCode;
+      },
+      set(newValue) {
+        this.$store.commit("setColorCode", newValue);
+      },
+    },
+  },
+
+  setup(props) {
+    const store = useStore();
+
+    store.commit("checkFun");
+  },
+};
 </script>
